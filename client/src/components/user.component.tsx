b0978a9b8ee9d54d9fd0b1e561ee676fb9c9f070
Anti Evo <jpujals@bellsouth.net>
2020-06-07 14:56:32 -0400

    Login page

--- client/src/components/user.component.tsx
+++ client/src/components/user.component.tsx
@@ -4,45 +4,12 @@ import { User } from '../models/User';
 import './user.component.css';
 import { Modal, Button, Form } from 'react-bootstrap';
 
+const loggedin = false;
+
 export const UserComponent: React.FC = () => {
     const [user, setUsers] = useState<User[]>([]);
 
-   
-    // We need to get data for our application
-    // So we should send a request to our API to acquire it
-    // 1. A get request is sent
-    // 2. Update state with new data
-    // 3. Component rerenders
 
-    // send get request
-    // will this work? -- This will send the request repeatedly, each state update causing
-    // a new request to be sent - DO NOT DO
-    // userRemote.getAllPeople().then(people => {
-    //     setUsers(people);
-    // });
-
-    /*
-        ! The Problem
-        Everytime the state updates component must reevaluated.  Because the request was
-        directly in the component's code, this caused the request to be sent again, which
-        triggered more and more state updates and more requests being sent. 
-    
-        The React component lifecycle - Lifecycle methods are methods which are called on
-        certain lifecycle stages for some process and are generally offered as mechanisms
-        to hook in custom functionality to an internal process.
-    */
-
-    /* 
-        ! The Solution - useEffect
-        useEffect is a lifecycle 'hook' and is part of a group of functions called 
-        React hooks (alongside useState). useEffect us used to describe some effect
-        that should take place as a side effect of the component rerendering. useEffect
-        can take in 1-2 arguments with the first argument being the intended effect. The
-        second argument takes an array of values and will reevaluate the effect if those
-        values change - this is intended to be a mechanism for the effect to intelligently
-        know when to reevaluate.  Note: For effects that should only happen once, we can
-        simply pass an [] to the second argument.
-    */
 
     useEffect(() => {
         loadUser();
@@ -56,7 +23,7 @@ export const UserComponent: React.FC = () => {
         });        
     }
 
-
+if(loggedin){
     return (
         <div>
             <header>
@@ -100,4 +67,23 @@ export const UserComponent: React.FC = () => {
            
         </div>
     );
+                }
+    else{
+        return (
+            <div>
+                <header>
+                    <h2 
+                    id="user-header" className="dark">Login                 
+                    </h2>
+                </header>
+    
+                {/* react-bootstrap components */}
+               
+    
+               
+            </div>
+        );
+    }
+
+
 }
